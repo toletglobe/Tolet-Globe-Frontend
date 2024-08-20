ef28d1adabe613aa93ae02a02b65a899fad55269
Raunak <raunakdob@gmail.com>
2024-08-20 18:16:25 +0530

    refactor: Improve image handling in Blog and BlogView components

--- src/config/config.js
+++ src/config/config.js
@@ -1,36 +1,35 @@
-import { BASE_URL } from "../constant/constant"
+import axios from 'axios';
+import { BASE_URL } from '../constant/constant';
 
 class Service {
-    static async fetchblog(){
-    try {
-        const response = await fetch(`${BASE_URL}blogs`,{
-            method:'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        const data = await response.json()
-        console.log(data)
-        return data
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
-    }
-    static async fetchBlogById(_id){
+    static async fetchBlog() {
         try {
-            const response = await fetch(`${BASE_URL}blogs/${_id}`,{
-                method:'GET',
+            const response = await axios.get(`${BASE_URL}blogs`, {
                 headers: {
                     'Content-Type': 'application/json',
-                }
-            })
-            const data = await response.json()
-            return data
+                },
+            });
+            console.log(response.data);
+            return response.data;
         } catch (error) {
-            console.log(error)
-            throw error
+            console.log(error);
+            throw error;
+        }
+    }
+
+    static async fetchBlogById(_id) {
+        try {
+            const response = await axios.get(`${BASE_URL}blogs/${_id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            throw error;
         }
     }
 }
-export default Service
+
+export default Service;
