20a40ec5480949741375d7526f41de52122262ea
Raunak <raunakdob@gmail.com>
2024-08-27 23:14:23 +0530

    chore: Add introductory text to BlogView and horizontal rule to Footer

--- src/components/blog/BlogView.jsx
+++ src/components/blog/BlogView.jsx
@@ -46,6 +46,9 @@ const BlogView = () => {
           </div>
         </div>
         <hr />
+        <div className="my-2 font-semibold">
+          {blog.intro}
+        </div>
         <div className=" mx-2 my-3">
           <img
             src={blog.image}
@@ -61,7 +64,6 @@ const BlogView = () => {
           ></div>
         </div>
         <br />
-        <hr />
       </div>
       {/* Assuming `content` contains HTML formatted string */}
     </div>
