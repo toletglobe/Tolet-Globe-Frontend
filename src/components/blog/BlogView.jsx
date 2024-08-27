c84aa97329d88f6bd4b6cdb2adf0cf5d06e7e5b3
Raunak <raunakdob@gmail.com>
2024-08-27 23:11:49 +0530

    chore: Update import paths and remove unnecessary code in BlogView component

--- src/components/blog/BlogView.jsx
+++ src/components/blog/BlogView.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Service from '../../config/config' // Adjust the import path accordingly
