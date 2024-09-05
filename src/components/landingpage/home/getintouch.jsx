3a872766ef6806e1646f922d40cc41475d685679
nazifshayan <141138402+nazifshayan@users.noreply.github.com>
2024-09-05 15:07:44 +0530

    Update getintouch.jsx
    Add map image

--- src/components/landingpage/home/getintouch.jsx
+++ src/components/landingpage/home/getintouch.jsx
@@ -5,56 +5,52 @@ export const ContactUs = () => {
   const googlemaps =
     'https://www.google.com/maps/place/To-Let+Globe/@26.8465566,80.9797793,15z/data=!4m6!3m5!1s0x399bfd77577ba78f:0xd2d6f22d1b246815!8m2!3d26.8465566!4d80.9797793!16s%2Fg%2F11vhrqqb45?entry=ttu'
   return (
-    <div className="my-10 h-full m-auto flex flex-col">
-      <div className="w-[80%] mx-auto h-full bg-black flex flex-col justify-between lg:flex-row lg:justify-between mt-15">
-        <a href={googlemaps} className="lg:w-[65%] flex h-[30rem] lg:h-auto">
+    <div className="my-10 h-[650px] m-auto flex flex-col mt-20">
+      <div className="w-[90%] mx-auto h-screen bg-black flex flex-col justify-between lg:flex-row lg:justify-between relative">
+        
+        <a href={googlemaps} className="lg:w-[65%] flex h-[85%] relative">
           <div
             style={{
-              backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 10%, transparent 10%, rgba(0, 0, 0, 0.4) 100%), 
-                               linear-gradient(to right, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.4) 30%, rgba(0, 0, 0, 0.4) 95%, rgba(0, 0, 0, 0.4) 100%), 
-                               url(${location})`,
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${location})`,
               backgroundSize: 'cover',
+              backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat',
             }}
-            className={` bg-cover bg-no-repeat flex flex-col items-center justify-center w-full h-[80%] text-white my-auto`}
+            className="flex flex-col justify-center items-center w-full h-full text-white"
           >
-            <p className="text-[#6CC1B6] mb-0 text-4xl font-bold"></p>
+            <p className="text-white text-[52px] font-bold mr-[500px] mt-[350px]">CONTACT US</p>
           </div>
         </a>
-
-        <div className="lg:w-[25%]  flex flex-col text-start lg:ml-8 my-10">
-          <p className="text-[#6CC1B6] font-normal text-4xl text-left pb-5">
+        <div className="lg:w-[35%] flex flex-col text-start lg:ml-8 my-10 relative z-10">
+          <p className="text-[#f5595a] font-bold text-4xl text-left pb-5">
             GET IN TOUCH
           </p>
-          <p className="text-gray-500 m-0 text-left pb-4">
-            Have some questions?
+          <p className="text-gray-400 text-left pb-4">
+            Have some questions?<br />
+            Feel free to ask them anytime
           </p>
-          <p className="text-gray-500 text-left m-0 pb-4">
-            feel free to ask them anytime
-          </p>
-
           <form className="w-full flex flex-col justify-between text-white">
             <input
               type="text"
               placeholder="Name"
-              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
             />
             <input
               type="email"
               placeholder="Email"
-              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
             />
             <input
               type="text"
               placeholder="Phone"
-              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
             />
             <input
               type="text"
               placeholder="Message"
-              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
             />
-            <button className="bg-none border-none text-[#c8a21c] text-left text-lg mt-4">
+            <button className="mr-[340px] text-white font-semibold bg-transparent mt-4 hover:text-[#f5595a]">
               Send Message &rarr;
             </button>
           </form>
