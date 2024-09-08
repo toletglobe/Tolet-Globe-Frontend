5a44bd58a095e46bf222bfa9bbc0ddd7640911d2
Toletglobe <toletglobetech@gmail.com>
2024-09-08 16:18:25 +0530

    Trial 2 for AWS Backend

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://to-let-backend.onrender.com/api/v1/";
+// export const BASE_URL = "https://to-let-backend.onrender.com/api/v1/";
 // export const BASE_URL = "http://localhost:8000/api/v1/";
-// export const BASE_URL = "http://ec2-43-205-177-89.ap-south-1.compute.amazonaws.com:8000/api/v1/";
+export const BASE_URL = "https://13.232.171.196:8000/api/v1/";
