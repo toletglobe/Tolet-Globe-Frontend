5d4cb26427601d1071580fc7f9d3eed0141ae6b3
shraddhaSarwade <shraddha154@gmail.com>
2024-09-08 22:18:16 +0530

    Fixed Contact Us, Get In Touch and Title Icon

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,46 @@
 
 import { BsChatRightDots } from "react-icons/bs";
 import { IoCallOutline } from "react-icons/io5";
+import { useState } from "react";
+import { API } from "../../config/axios";
+import { toast } from "react-hot-toast";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    msg: "",
+    topic: "",
+  });
+
+  const handleChange = (evt) => {
+    setFormData((prev) => ({
+      ...prev,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+
+  const handleReset = () => {
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      msg: "",
+      topic: "",
+    });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault(); // Prevent the default form submissio
+
+    const dataForEnquiry = formData;
+    handleReset();
+    toast.success("Enquiry Sent! We will get in touch with you.");
+    const response2 = await API.post("contact/submit-data", dataForEnquiry);
+    console.log(response2);
+  };
+
   return (
     <div className="flex flex-col md:flex-row md:gap-9 p-5 mx-6 my-10 justify-between mt-20">
       <div className="w-full md:w-1/2 px-5 mt-[50px]">
@@ -21,83 +59,99 @@ const Contact = () => {
           </p>
         </div>
 
-        
-         <div className="flex flex-row gap-5 mt-20">
-            <BsChatRightDots className="text-white text-4xl" />
-            <div>
-              <h1 className="text-white text-2xl">Chat with us !!</h1>
-              <p className="text-gray-300/50 mt-1">
-                Our friendly team is here to help
-              </p>
-              <p className="text-[#6CC1B6] ">to_let@gmail.com</p>
-            </div>
-         </div>
-          <div className="flex flex-row gap-5 mt-10">
-            <IoCallOutline className="text-white text-4xl" />
-            <div>
-              <h1 className="text-white text-2xl">Call us...</h1>
-              <p className="text-gray-300/50 mt-1">
-              Mon - fri 8 am to 10 pm
-              </p>
-              <p className="text-[#6CC1B6] ">+91 9876543210</p>
-            </div>
+        <div className="flex flex-row gap-5 mt-20">
+          <BsChatRightDots className="text-white text-4xl" />
+          <div>
+            <h1 className="text-white text-2xl">Chat with us !!</h1>
+            <p className="text-gray-300/50 mt-1">
+              Our friendly team is here to help
+            </p>
+            <p className="text-[#6CC1B6] ">toletglobetech@gmail.com</p>
           </div>
         </div>
+        <div className="flex flex-row gap-5 mt-10">
+          <IoCallOutline className="text-white text-4xl" />
+          <div>
+            <h1 className="text-white text-2xl">Call us...</h1>
+            <p className="text-gray-300/50 mt-1">Mon - Fri, 8 AM to 10 PM</p>
+            <p className="text-[#6CC1B6] ">+91 9876543210</p>
+          </div>
+        </div>
+      </div>
 
       <div className="flex flex-col gap-3 w-full md:w-1/2 px-5 mt-5 md:mt-0">
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="flex flex-col">
-            <label htmlFor="topix" className="text-gray-400">
+            <label htmlFor="topic" className="text-gray-400">
               Topic
             </label>
-            <select className="mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800">
+            <select
+              className="mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              id="topic"
+              value={formData.topic}
+              name="topic"
+              onChange={handleChange}
+            >
               <option value="">Select a topic</option>
-              <option value="RH">Rental House</option>
-              <option value="RF">Rental Flat</option>
-              <option value="PG">PGs</option>
-              <option value="GD">Godowns</option>
+              <option value="Residential">Residential</option>
+              <option value="Commercial">Commercial</option>
+              <option value="Others">Others</option>
             </select>
           </div>
           <div className="mt-5 flex flex-col">
-            <label htmlFor="topix" className="text-gray-400">
+            <label htmlFor="name" className="text-gray-400">
               Name
             </label>
             <input
               type="text"
-              id="topic"
-              placeholder="johndoe"
+              id="name"
+              placeholder="John Doe"
+              value={formData.name}
+              name="name"
               className=" mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              onChange={handleChange}
             />
           </div>
           <div className="mt-5 flex flex-col">
-            <label htmlFor="topix" className="text-gray-400">
+            <label htmlFor="email" className="text-gray-400">
               Email
             </label>
             <input
-              type="mail"
-              id="Email"
+              type="email"
+              id="email"
               placeholder="name@provider.com"
+              value={formData.email}
+              name="email"
               className=" mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              onChange={handleChange}
             />
           </div>
           <div className="mt-5 flex flex-col">
-            <label htmlFor="topix" className="text-gray-400">
+            <label htmlFor="phone" className="text-gray-400">
               Phone No.
             </label>
             <input
-              type="phone"
+              type="text"
               id="phone"
-              placeholder="+995 598 63 79 48"
-              className=" mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              placeholder="+91 8945678323"
+              value={formData.phone}
+              name="phone"
+              className="mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              onChange={handleChange}
             />
           </div>
           <div className="mt-5 flex flex-col">
-            <label className="text-gray-400">Message</label>
+            <label htmlFor="msg" className="text-gray-400">
+              Message
+            </label>
             <textarea
               type="text"
               id="msg"
               placeholder="Type your message..."
+              value={formData.msg}
+              name="msg"
               className=" row-span-6 mt-2 appearance-none border bg-transparent border-gray-300 rounded-lg w-full py-3 px-4 text-gray-400 leading-tight focus:outline-none focus:ring-2 focus:ring-green-800"
+              onChange={handleChange}
             />
           </div>
           <button className="mt-6 bg-[#6CC1B6] w-full text-black py-3 px-4 rounded-lg">
--- src/components/landingpage/home/getintouch.jsx
+++ src/components/landingpage/home/getintouch.jsx
@@ -1,61 +1,133 @@
-import 'animate.css'
-import location from '../../../assets/getintouch/toletglobelocationimage.jpg'
+import "animate.css";
+import location from "../../../assets/getintouch/toletglobelocationimage.jpg";
+import { useState } from "react";
+import { API } from "../../../config/axios";
+import { toast } from "react-hot-toast";
 
 export const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    msg: "",
+    topic: "",
+  });
+
+  const handleChange = (evt) => {
+    setFormData((prev) => ({
+      ...prev,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+
+  const handleReset = () => {
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      msg: "",
+      topic: "",
+    });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault(); // Prevent the default form submissio
+    const dataForEnquiry = formData;
+    handleReset();
+    toast.success("Enquiry Sent! We will get in touch with you.");
+    const response2 = await API.post("contact/submit-data", dataForEnquiry);
+    console.log(response2);
+  };
+
   const googlemaps =
-    'https://www.google.com/maps/place/To-Let+Globe/@26.8465566,80.9797793,15z/data=!4m6!3m5!1s0x399bfd77577ba78f:0xd2d6f22d1b246815!8m2!3d26.8465566!4d80.9797793!16s%2Fg%2F11vhrqqb45?entry=ttu'
+    "https://www.google.com/maps/place/To-Let+Globe/@26.8465566,80.9797793,15z/data=!4m6!3m5!1s0x399bfd77577ba78f:0xd2d6f22d1b246815!8m2!3d26.8465566!4d80.9797793!16s%2Fg%2F11vhrqqb45?entry=ttu";
   return (
-    <div className="my-10 h-[650px] m-auto flex flex-col mt-20">
-      <div className="w-[90%] mx-auto h-screen bg-black flex flex-col justify-between lg:flex-row lg:justify-between relative">
-        
-        <a href={googlemaps} className="lg:w-[65%] flex h-[85%] relative">
+    <div className="my-10 h-full m-auto flex flex-col">
+      <div className="w-[80%] mx-auto h-full bg-black flex flex-col justify-between lg:flex-row lg:justify-between mt-15">
+        <a href={googlemaps} className="lg:w-[65%] flex h-[30rem] lg:h-auto">
           <div
             style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${location})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
+              backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 10%, transparent 10%, rgba(0, 0, 0, 0.4) 100%), 
+                         linear-gradient(to right, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.4) 30%, rgba(0, 0, 0, 0.4) 95%, rgba(0, 0, 0, 0.4) 100%), 
+                         url(${location})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
             }}
-            className="flex flex-col justify-center items-center w-full h-full text-white"
+            className={` bg-cover bg-no-repeat flex flex-col items-center justify-center w-full h-[80%] text-white my-auto`}
           >
-            <p className="text-white text-[52px] font-bold mr-[500px] mt-[350px]">CONTACT US</p>
+            <p className="text-[#6CC1B6] mb-0 text-4xl font-bold"></p>
           </div>
         </a>
-        <div className="lg:w-[35%] flex flex-col text-start lg:ml-8 my-10 relative z-10">
-          <p className="text-[#f5595a] font-bold text-4xl text-left pb-5">
+
+        <div className="lg:w-[25%]  flex flex-col text-start lg:ml-8 my-10">
+          <p className="text-[#6CC1B6] font-normal text-4xl text-left pb-5">
             GET IN TOUCH
           </p>
-          <p className="text-gray-400 text-left pb-4">
-            Have some questions?<br />
-            Feel free to ask them anytime
+          <p className="text-gray-500 m-0 text-left pb-4">
+            Have some questions?
           </p>
-          <form className="w-full flex flex-col justify-between text-white">
+          <p className="text-gray-500 text-left m-0 pb-4">
+            feel free to ask them anytime
+          </p>
+
+          <form
+            className="w-full flex flex-col justify-between text-white"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col">
+              <select
+                className=" bg-black border-b-2 border-gray-400 text-gray-400 focus:outline-none my-6 "
+                id="topic"
+                value={formData.topic}
+                name="topic"
+                onChange={handleChange}
+              >
+                <option className=" text-black" value="">
+                  Topic
+                </option>
+                <option value="Residential">Residential</option>
+                <option value="Commercial">Commercial</option>
+                <option value="Others">Others</option>
+              </select>
+            </div>
             <input
               type="text"
               placeholder="Name"
-              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
+              value={formData.name}
+              name="name"
+              className=" bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              onChange={handleChange}
             />
             <input
               type="email"
               placeholder="Email"
-              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
+              value={formData.email}
+              name="email"
+              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              onChange={handleChange}
             />
             <input
               type="text"
               placeholder="Phone"
-              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
+              value={formData.phone}
+              name="phone"
+              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              onChange={handleChange}
             />
             <input
               type="text"
               placeholder="Message"
-              className="placeholder-white bg-transparent border-b border-gray-600 text-white focus:outline-none my-6 pb-2"
+              value={formData.msg}
+              name="msg"
+              className="bg-black border-b-2 border-gray-400 text-white focus:outline-none my-6"
+              onChange={handleChange}
             />
-            <button className="mr-[340px] text-white font-semibold bg-transparent mt-4 hover:text-[#f5595a]">
+            <button className="bg-none border-none text-[#c8a21c] text-left text-lg mt-4">
               Send Message &rarr;
             </button>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
