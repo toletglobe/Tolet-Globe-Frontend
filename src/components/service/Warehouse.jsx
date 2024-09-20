98288fd501e9357cae8ce0be423ead4b3851093d
1911yogesh <yogeshsahu2516@gmail.com>
2024-09-20 13:16:00 +0530

    service components created

---
+++ src/components/service/Warehouse.jsx
@@ -0,0 +1,10 @@
+const Warehouse = () => {
+    return (
+      <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">Warehouse</h1>
+      </div>
+    );
+  };
+  
+  export default Warehouse;
+  
