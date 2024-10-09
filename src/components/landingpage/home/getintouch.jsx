3a070267a1192336334afa7dd237078616b66025
Tarun Tiwari <tiwaritarun700@gmail.com>
2024-10-09 23:02:12 +0530

    getInTouch segment updated

--- src/components/landingpage/home/getintouch.jsx
+++ src/components/landingpage/home/getintouch.jsx
@@ -1,5 +1,5 @@
 import "animate.css";
-import location from "../../../assets/getintouch/toletglobelocationimage.jpg";
+import location from "../../../assets/map.jpg";
 import { useRef, useState } from "react";
 import { API } from "../../../config/axios";
 import { toast } from "react-hot-toast";
@@ -54,36 +54,34 @@ export const ContactUs = () => {
   const googlemaps =
     "https://www.google.com/maps/place/To-Let+Globe/@26.8465566,80.9797793,15z/data=!4m6!3m5!1s0x399bfd77577ba78f:0xd2d6f22d1b246815!8m2!3d26.8465566!4d80.9797793!16s%2Fg%2F11vhrqqb45?entry=ttu";
   return (
-    <div className="my-10 h-full m-auto flex flex-col">
-      <div className="w-full mx-auto h-full bg-black flex flex-col justify-between lg:flex-row lg:justify-between mt-15 px-20">
-        <a href={googlemaps} className="lg:w-[65%] flex h-[30rem] lg:h-auto">
-          <div
-            style={{
-              backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 10%, transparent 10%, rgba(0, 0, 0, 0.4) 100%), 
-                         linear-gradient(to right, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.4) 30%, rgba(0, 0, 0, 0.4) 95%, rgba(0, 0, 0, 0.4) 100%), 
-                         url(${location})`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-            }}
-            className={` bg-cover bg-no-repeat flex flex-col items-center justify-center w-full h-[80%] text-white my-auto`}
-          >
-            <p className="text-[#6CC1B6] mb-0 text-4xl font-bold"></p>
-          </div>
-        </a>
-
-        <div className="lg:w-[25%]  flex flex-col text-start lg:ml-8 my-10">
-          <p className="text-[#6CC1B6] font-normal text-4xl text-left pb-5">
+   
+    <div  className="my-10 mx-20 lg:h-[85vh] flex lg:justify-end "    
+     style={{
+      backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.6) 0%, transparent 20%, transparent 80%, rgba(0, 0, 0, 0.6) 100%), 
+      linear-gradient(to right, rgba(0, 0, 0, 0.6) 0%, transparent 20%, transparent 80%, rgba(0, 0, 0, 0.6) 100%), 
+      url(${location})`,
+                
+              }}>
+       <a href={googlemaps} >  
+        <div className="text-star  h-[100%] lg:w-[860px] flex items-end ">
+            <div className="text-white font-semibold lg:text-5xl text-left pb-5 mb-10 -ml-20 text-shadow-lg -stroke-2 -stroke-white lg:block hidden  ">
+              CONTACT US
+            </div>
+          </div>        
+      </a>
+        <div className=" text-start bg-black bg-opacity-80 p-10  lg:h-[100%] lg:w-[380px] w-full  ">
+          <p className="text-yellow-300 font-normal text-4xl text-left pb-5">
             GET IN TOUCH
           </p>
           <p className="text-gray-500 m-0 text-left pb-4">
             Have some questions?
           </p>
           <p className="text-gray-500 text-left m-0 pb-4">
-            feel free to ask them anytime
+            Feel free to ask them anytime
           </p>
 
           <form 
-            className="w-full flex flex-col justify-between text-white"
+            className="w-full flex flex-col justify-between text-white "
             onSubmit={handleSubmit}
           >
             <div className="flex flex-col">
@@ -139,7 +137,7 @@ export const ContactUs = () => {
               required
             />
             {loading ? (
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 bg-black bg-opacity-70">
               <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>
               <span className="text-yellow-300">Sending...</span>
             </div>
@@ -152,7 +150,6 @@ export const ContactUs = () => {
           )} 
           </form>
         </div>
-      </div>
     </div>
   );
 };
