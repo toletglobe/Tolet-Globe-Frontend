5dc9bd94d5f83827b49556c26fc52c3579fb396b
Rekha Kaushik <139316962+Rekha21k@users.noreply.github.com>
2024-10-11 18:20:56 +0530

    update PropertyCarousel.jsx

--- src/components/landingpage/home/PropertyCarousel.jsx
+++ src/components/landingpage/home/PropertyCarousel.jsx
@@ -6,6 +6,32 @@ import image2 from "../../../assets/property/img2.svg";
 import image3 from "../../../assets/property/img3.svg";
 import image4 from "../../../assets/property/img4.svg";
 
+// Custom Next Arrow
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute right-[-23px] top-1/2 transform -translate-y-1/2 z-10 cursor-pointer text-white text-[50px]"
+      onClick={onClick}
+    >
+      &gt;
+    </div>
+  );
+};
+
+// Custom Previous Arrow
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className="absolute left-[-23px] top-1/2 transform -translate-y-1/2 z-10 cursor-pointer text-white text-[50px]"
+      onClick={onClick}
+    >
+      &lt;
+    </div>
+  );
+};
+
 export const PropertyCarousel = () => {
   const settings = {
     dots: true,
@@ -15,27 +41,28 @@ export const PropertyCarousel = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    nextArrow: <NextArrow />, // Use custom right arrow
+    prevArrow: <PrevArrow />, // Use custom left arrow
   };
-
   return (
     <div className="bg-black text-white flex items-center justify-center flex-col p-5 mt-10 mb-10 w-full">
       <div className="flex items-center justify-center flex-col gap-3 w-full my-5 mb-8">
         {/* Heading */}
-        <h1 className="text-4xl text-[#6CC1B6]  font-bold">Top Locations</h1>
+        <h1 className="text-4xl text-[#6CC1B6] font-bold">Top Locations</h1>
         {/* Subheading */}
         <p className="w-full md:w-2/4 text-[14px] text-[#CCB454] text-center">
           We proudly offer our services in these major cities, having successfully connected with numerous satisfied members along the way.
         </p>
       </div>
-
+  
       <Slider {...settings} className="w-full px-3 md:px-8 lg:px-1">
         {/* Slide 1: Lucknow */}
         <div className="flex flex-col md:flex-row items-center justify-between gap-8">
-          {/* Image */}
+          {/* Image (on the left side) */}
           <div className="w-full md:w-1/2">
             <img src={image1} alt="Property in Lucknow" className="w-full h-auto" />
           </div>
-          {/* Text */}
+          {/* Text (on the right side) */}
           <div className="w-full md:w-1/2 flex flex-col gap-4 md:gap-6 items-start">
             <h2 className="text-[32px] md:text-[40px] font-normal">Find the best To-Let in Lucknow</h2>
             <p className="text-[#CCB454]">
@@ -46,14 +73,14 @@ export const PropertyCarousel = () => {
             </Link>
           </div>
         </div>
-
+  
         {/* Slide 2: Ayodhya */}
         <div className="flex flex-col md:flex-row items-center justify-between gap-8">
-          {/* Image */}
+          {/* Image (on the left side) */}
           <div className="w-full md:w-1/2">
             <img src={image2} alt="Property in Ayodhya" className="w-full h-auto" />
           </div>
-          {/* Text */}
+          {/* Text (on the right side) */}
           <div className="w-full md:w-1/2 flex flex-col gap-4 md:gap-6 items-start">
             <h2 className="text-[32px] md:text-[40px] font-normal">Find the best To-Let in Ayodhya</h2>
             <p className="text-[#CCB454]">
@@ -64,14 +91,14 @@ export const PropertyCarousel = () => {
             </Link>
           </div>
         </div>
-
+  
         {/* Slide 3: Vellore */}
         <div className="flex flex-col md:flex-row items-center justify-between gap-8">
-          {/* Image */}
+          {/* Image (on the left side) */}
           <div className="w-full md:w-1/2">
             <img src={image3} alt="Property in Vellore" className="w-full h-auto" />
           </div>
-          {/* Text */}
+          {/* Text (on the right side) */}
           <div className="w-full md:w-1/2 flex flex-col gap-4 md:gap-6 items-start">
             <h2 className="text-[32px] md:text-[40px] font-normal">Find the best To-Let in Vellore</h2>
             <p className="text-[#CCB454]">
@@ -82,14 +109,14 @@ export const PropertyCarousel = () => {
             </Link>
           </div>
         </div>
-
+  
         {/* Slide 4: Kota */}
         <div className="flex flex-col md:flex-row items-center justify-between gap-8">
-          {/* Image */}
+          {/* Image (on the left side) */}
           <div className="w-full md:w-1/2">
             <img src={image4} alt="Property in Kota" className="w-full h-auto" />
           </div>
-          {/* Text */}
+          {/* Text (on the right side) */}
           <div className="w-full md:w-1/2 flex flex-col gap-4 md:gap-6 items-start">
             <h2 className="text-[32px] md:text-[40px] font-normal">Find the best To-Let in Kota</h2>
             <p className="text-[#CCB454]">
