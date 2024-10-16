ae77e1c3cb4ff3dceccb4f965ff3f5b8babf98ca
SaiRohith202001 <sairohith202001@gmail.com>
2024-10-16 17:37:34 +0530

    updated hiring partner

--- src/components/Hiringpartner/HiringPartners.jsx
+++ src/components/Hiringpartner/HiringPartners.jsx
@@ -14,7 +14,7 @@ const HiringPartners = () => {
     <div className="h-full w-full py-8 px-6 bg-black flex flex-col items-center justify-center relative -top-8 border border-white rounded-lg shadow-lg" 
          style={{ width: '1212px', height: '246px', marginLeft: 'auto', marginRight: '160px' }}> {/* Align component to the right */}
       <div className="max-w-[1310px] w-full text-center">
-        <div className="bg-black z-20 relative inline-block" style={{ width: '392px', height: '49px', borderRadius: '10px', position: 'absolute', left: '410px',top:-20 }}> {/* Updated styles */}
+        <div className="bg-black z-20 relative inline-block" style={{ width: '392px', height: '49px', borderRadius: '10px', position: 'absolute', left: '410px',top:-24 }}> {/* Updated styles */}
           <h2 className="text-[#2e7766] text-4xl font-bold mb-0 relative">
             Our Hiring Partners
           </h2>
@@ -23,7 +23,7 @@ const HiringPartners = () => {
           <img
             src={linkedinLogo}
             alt="LinkedIn"
-            onClick={() => handleLogoClick("https://linkedin.com")}
+            onClick={() => handleLogoClick("https://www.linkedin.com/company/to-let-globe/posts/?feedView=all")}
             className="cursor-pointer h-[85px] max-w-[200px] rounded-[5px] transition-transform ease-in-out duration-300 hover:scale-105 hover:opacity-90"
           />
           <img
@@ -41,7 +41,7 @@ const HiringPartners = () => {
           <img
             src={pregradLogo}
             alt="Pregrad"
-            onClick={() => handleLogoClick("https://pregrad.com")}
+            onClick={() => handleLogoClick("https://www.pregrad.in/")}
             className="cursor-pointer h-[85px] max-w-[200px] rounded-[5px] transition-transform ease-in-out duration-300 hover:scale-105 hover:opacity-90"
           />
           <img
