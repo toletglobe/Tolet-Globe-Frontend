82354b39ca88cf6066fc1b9e890326b75a9e86ae
purniiiima <purniiiima@gmail.com>
2024-10-18 19:14:32 +0530

    updated property status

--- src/components/property/listingComponents/Cards.jsx
+++ src/components/property/listingComponents/Cards.jsx
@@ -88,23 +88,23 @@ const Cards = ({
                 </div>
               )}
               <div
-              className="card-badge-left absolute top-6 left-6 text-white text-xs uppercase px-3 py-1"
-              style={{
-                backgroundColor:
-                  property.availabilityStatus === 'Available'
+                className="card-badge-left absolute top-6 left-6 text-white text-xs uppercase px-3 py-1"
+                style={{
+                  backgroundColor:
+                    property.availabilityStatus === 'Available'
                     ? '#236b62' // Green for available
                     : property.availabilityStatus === 'Rented Out'
                     ? '#c71221' // Red for rented
                     : '#999999', // Gray for not available (NA)
-                textTransform: 'capitalize',
-              }}
+                  textTransform: 'capitalize',
+                }}
               >
-              {property.availabilityStatus === 'Available'
-                ? 'Available'
-                : property.availabilityStatus === 'Rented Out'
-                ? 'Rented'
-                : 'NA'}
-               </div>
+                {property.availabilityStatus === 'Available'
+                  ? 'Available'
+                  : property.availabilityStatus === 'Rented Out'
+                  ? 'Rent Out'
+                  : 'NA'}
+              </div>
 
               <div className="banner-actions absolute bottom-4 left-4 right-4 flex gap-4 justify-between">
                 <div>
