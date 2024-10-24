e4a9996e11dae44bb068cedc8b822ecaa2c2af78
Octochic <sakshikharade.work@gmail.com>
2024-10-24 21:12:54 +0530

    responsive navbar, fixinf logo size, menu button, dropdown on mobile view

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import userIcon from "../../assets/user-icon.png"; // Fallback image
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/authSlice";
+import {
+  ArrowLeftStartOnRectangleIcon,
+  ComputerDesktopIcon,
+} from "@heroicons/react/24/outline";
 
-const NavBar = () => {
+const NavBar = ({ userInfo }) => {
   const [activeLink, setActiveLink] = useState("");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const authState = useSelector((state) => state.auth);
@@ -15,18 +20,26 @@ const NavBar = () => {
 
   const handleNavLinkClick = (link) => {
     setActiveLink(link);
-    setIsMenuOpen(false); // Close the dropdown when a nav link is clicked
+    localStorage.setItem("activeLink", link);
+    setIsMenuOpen(false); // Close dropdown after selecting an option
   };
 
+  useEffect(() => {
+    const savedLink = localStorage.getItem("activeLink");
+    if (savedLink) {
+      setActiveLink(savedLink);
+    }
+  }, []);
+
   const handleLogout = () => {
+    setIsMenuOpen(false);
     localStorage.removeItem("token");
     dispatch(logout());
     navigate("/login");
     toast.success("Logged out!");
-    setIsMenuOpen(false); // Close the dropdown when logging out
   };
 
-  // Handling click outside avatar dropdown
+  // Handling click outside event for avatar
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (avatarRef.current && !avatarRef.current.contains(event.target)) {
@@ -35,145 +48,63 @@ const NavBar = () => {
     };
 
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [avatarRef]);
 
-  const handleDropdownSelection = (path) => {
-    navigate(path); // Navigate to the desired path
-    setIsMenuOpen(false); // Close the dropdown when a selection is made
-  };
-
   return (
     <nav className="z-50">
-      <div className="w-full bg-black top-0 flex justify-between fixed items-center px-5 lg:px-20 py-8">
-        {/* Logo on the left */}
-        <div className="flex items-center">
+      <div className="w-full bg-black top-0 flex justify-between fixed items-center px-4 lg:px-20 py-4">
+        {/* Logo on the left corner */}
+        <div className="navbar-logo">
           <Link to="/" className="flex items-center">
-            <img
-              src={logo}
-              alt="Logo"
-              className="h-12 lg:h-16" // Larger on web and smaller on mobile
-            />
+            <img src={logo} alt="Logo" className="h-16 lg:h-12 ml-0" />
           </Link>
         </div>
 
-        {/* Navigation buttons on the right side for desktop */}
-        <div className="hidden lg:flex space-x-6 items-center ml-24"> {/* Added ml-24 for fixed large space between logo and buttons */}
-          <Link
-            to="/"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "home" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("home")}
-          >
-            Home
-          </Link>
-          <Link
-            to="/service"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "service" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("service")}
-          >
-            Service
-          </Link>
-          <Link
-            to="/blog"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "blog" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("blog")}
-          >
-            Blog
-          </Link>
-          <Link
-            to="/contact"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "contact" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("contact")}
-          >
-            Contact
-          </Link>
-          <Link
-            to="/aboutus"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "aboutus" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("aboutus")}
-          >
-            About
-          </Link>
-          <Link
-            to="/property"
-            className={`block px-5 text-gray-300 ${
-              activeLink === "propertyListing" ? "text-white bg-teal-500 rounded-md" : ""
-            }`}
-            onClick={() => handleNavLinkClick("propertyListing")}
-          >
-            Property Listing
-          </Link>
-          {authState.status === true && localStorage.getItem("token") ? (
-            <Link
-              to="/landlord-dashboard"
-              className={`block px-5 text-gray-300 ${
-                activeLink === "dashboard" ? "text-white bg-teal-500 rounded-md" : ""
-              }`}
-              onClick={() => handleNavLinkClick("dashboard")}
+        {/* Menu button on the right corner for mobile view */}
+        <div className="flex flex-row">
+          <div className="flex justify-end">
+            <button
+              className="text-white block lg:hidden"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-              Dashboard
-            </Link>
-          ) : (
-            <Link
-              to="/login"
-              className={`block px-5 text-gray-300 ${
-                activeLink === "login" ? "text-white bg-teal-500 rounded-md" : ""
-              }`}
-              onClick={() => handleNavLinkClick("login")}
-            >
-              Login
-            </Link>
-          )}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
 
-        {/* Mobile menu toggle button */}
-        <div className="lg:hidden flex items-center">
-          <button
-            className="text-white"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
-          </button>
-        </div>
-
-        {/* Mobile dropdown menu */}
+        {/* Navbar items */}
         <div
+          id="basic-navbar-nav"
           className={`${
             isMenuOpen ? "block" : "hidden"
-          } lg:hidden absolute top-24 right-0 bg-black w-full p-4 z-40`}
+          } lg:flex lg:items-center lg:w-auto w-full absolute lg:static top-full left-0 bg-black lg:bg-transparent`}
         >
-          <ul className="flex flex-col space-y-4">
+          <ul className="lg:flex lg:items-center lg:justify-between text-base text-gray-300 pt-1 lg:pt-0">
             <li>
               <Link
                 to="/"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/")}
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "home"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("home")}
               >
                 Home
               </Link>
@@ -181,8 +112,12 @@ const NavBar = () => {
             <li>
               <Link
                 to="/service"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/service")}
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "service"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("service")}
               >
                 Service
               </Link>
@@ -190,8 +125,12 @@ const NavBar = () => {
             <li>
               <Link
                 to="/blog"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/blog")}
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "blog"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("blog")}
               >
                 Blog
               </Link>
@@ -199,8 +138,12 @@ const NavBar = () => {
             <li>
               <Link
                 to="/contact"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/contact")}
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "contact"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("contact")}
               >
                 Contact
               </Link>
@@ -208,42 +151,84 @@ const NavBar = () => {
             <li>
               <Link
                 to="/aboutus"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/aboutus")}
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "aboutus"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("aboutus")}
               >
                 About
               </Link>
             </li>
             <li>
               <Link
-                to="/property"
-                className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                onClick={() => handleDropdownSelection("/property")}
+                to="/property-listing"
+                className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 mx-2 ${
+                  activeLink === "propertyListing"
+                    ? "text-white bg-teal-500 rounded-md"
+                    : ""
+                }`}
+                onClick={() => handleNavLinkClick("propertyListing")}
               >
                 Property Listing
               </Link>
             </li>
-            {authState.status === true && localStorage.getItem("token") ? (
-              <li>
-                <Link
-                  to="/landlord-dashboard"
-                  className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                  onClick={() => handleDropdownSelection("/landlord-dashboard")}
-                >
-                  Dashboard
-                </Link>
-              </li>
-            ) : (
-              <li>
+            <li>
+              {/* User avatar or login button */}
+              {authState.status === true && localStorage.getItem("token") ? (
+                <div ref={avatarRef}>
+                  <button
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    className="rounded-full relative flex justify-center items-center w-9 h-9 mx-3 text-white bg-teal-500"
+                  >
+                    <img
+                      src={userInfo?.profilePicture || userIcon} // Use fallback image if profilePicture is undefined
+                      alt="avatar"
+                      className="w-full h-full rounded-full p-1"
+                    />
+                  </button>
+                  {isMenuOpen && (
+                    <div className="absolute top-20 right-14 w-fit h-fit flex flex-col justify-center items-center text-[#120404] bg-white rounded-lg">
+                      <ul className="w-full flex flex-col items-start">
+                        <li className="font-extrabold p-3 w-full text-center bg-gray-200 ">
+                          {authState.userData?.firstName?.toUpperCase() || "User"}
+                        </li>
+                        <li
+                          onClick={() => {
+                            navigate("/landlord-dashboard");
+                            setIsMenuOpen(false);
+                          }}
+                          className="w-full cursor-pointer flex items-center p-3 hover:bg-gray-200 rounded-lg"
+                        >
+                          <ComputerDesktopIcon className="w-[18px] h-[18px] mr-2" />{" "}
+                          Dashboard
+                        </li>
+                        <li
+                          className="w-full cursor-pointer flex items-center p-3 hover:bg-gray-200 rounded-lg"
+                          onClick={handleLogout}
+                        >
+                          <ArrowLeftStartOnRectangleIcon className="w-[18px] h-[18px] mr-2" />{" "}
+                          Logout
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              ) : (
                 <Link
                   to="/login"
-                  className={`block px-5 text-gray-300 hover:bg-teal-500 hover:text-white rounded-md`}
-                  onClick={() => handleDropdownSelection("/login")}
+                  className={`block px-5 py-2 lg:inline-block mt-4 lg:mt-0 ${
+                    activeLink === "login"
+                      ? "text-white bg-teal-500 rounded-md"
+                      : ""
+                  }`}
+                  onClick={() => handleNavLinkClick("login")}
                 >
                   Login
                 </Link>
-              </li>
-            )}
+              )}
+            </li>
           </ul>
         </div>
       </div>
