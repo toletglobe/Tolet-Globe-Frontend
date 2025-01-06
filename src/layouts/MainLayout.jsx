8629874a0410f76b01085aca6836efa8413c2028
shraddhaSarwade <shraddha154@gmail.com>
2025-01-06 15:36:46 +0530

    Made Changes to Import for Navbar Component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/Navbar/Navbar.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 
 const MainLayout = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <NavBar />
+        <Navbar />
         <main>
           <Outlet />
         </main>
