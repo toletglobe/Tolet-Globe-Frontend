1593250b5534fdfcefd4228e154f1e8502bc2109
Bhanutej73 <bhanutej930@gmail.com>
2025-01-06 19:04:16 +0530

    Changes BlogItem for blog page responsiveness

--- src/pages/blog/components/card/BlogItem.jsx
+++ src/pages/blog/components/card/BlogItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Img from './Img';
 import BlogBody from './BlogBody';
 import ReadMore from './ReadMore';
@@ -10,25 +10,26 @@ function BlogItem({ index, data, handleViewBlog }) {
     return (
         <div
             key={index}
-            className="text-white rounded-xl m-5 h-auto flex flex-col justify-center pb-2 border-b-4 border-stone-500 hover:border-[#6CC1B6]"
+            className="text-white rounded-xl h-auto flex flex-col justify-center pb-2 border-b-4 border-stone-500 hover:border-[#6CC1B6]"
         >
             <div className="">
-                <Img data={data} handleViewBlog={handleViewBlog}/>
+                <Img data={data} handleViewBlog={handleViewBlog} />
                 <div className="p-2">
                     <div className="mt-2">
                         <span className="text-gray-300 font-semibold">
                             {new Date(data?.date).toDateString()}
                         </span>
-                        {/* <Date data={data} /> */}
-                        {" "}
-                        | <span className="underline">{data?.category}</span>
+                        {" "} | <span className="underline">{data?.category}</span>
                     </div>
-                    <BlogBody data={data} handleViewBlog={handleViewBlog}/>
+                    <BlogBody data={data} handleViewBlog={handleViewBlog} />
                     <ReadMore data={data} handleViewBlog={handleViewBlog} />
 
-                    <div className="mt-4 flex justify-between items-center">
+                    {/* Footer Section */}
+                    <div className="mt-4">
+                        {/* Author */}
                         <Author data={data} />
-                        <div className="flex flex-row gap-5">
+                        {/* Likes and Views */}
+                        <div className="flex flex-row gap-5 mt-2">
                             <Views data={data} />
                             <Likes data={data} />
                         </div>
@@ -36,7 +37,7 @@ function BlogItem({ index, data, handleViewBlog }) {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default BlogItem
+export default BlogItem;
