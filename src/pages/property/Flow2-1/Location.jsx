af5db72cd36636fb8a96fc76dc1885039a1c8250
samyak-19 <141000887+samyak-19@users.noreply.github.com>
2025-03-06 00:50:14 +0530

    Fix the Review component

--- src/pages/property/Flow2-1/Location.jsx
+++ src/pages/property/Flow2-1/Location.jsx
@@ -70,7 +70,7 @@ const LocationComponent = ({ property, selectComp }) => {
         <div className="lg:flex lg:space-x-4 justify-between lg:mx-10">
             {/* Map */}
         <div className="lg:w-[74%] w-full h-64 lg:h-[550px]">
-        <div className="w-full lg:w-[60%] xl:w-[67%] 2xl:w-[69%] h-64 lg:h-[550px]  absolute backdrop-blur-sm bg-black/40 flex justify-center items-center"><RiLock2Fill color="#ffffff" size={30} /></div>
+        <div className="w-full lg:w-[60%] xl:w-[69%]  h-64 lg:h-[550px]  absolute backdrop-blur-sm bg-black/40 flex justify-center items-center"><RiLock2Fill color="#ffffff" size={30} /></div>
           <iframe
             src={getMapSrc()}
             width="100%"
