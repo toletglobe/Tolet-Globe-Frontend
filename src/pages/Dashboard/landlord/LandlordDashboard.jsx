7595b93b8d96d587d2e1d30871801e153b1fab21
deepanshu921372 <124592629+deepanshu921372@users.noreply.github.com>
2025-03-27 21:52:59 +0530

    updated code

--- src/pages/Dashboard/landlord/LandlordDashboard.jsx
+++ src/pages/Dashboard/landlord/LandlordDashboard.jsx
@@ -4,12 +4,12 @@ import LandlordDashboardSidebar from "./LandlordDashboardSidebar";
 export default function LandlordDashboard() {
   return (
     <>
-      <div className="w-[100vw] mt-16 flex flex-col min-[320px]:max-sm:justify-center min-[320px]:max-sm:items-center gap-x-2 sm:flex-row xl:ml-12">
+      <div className="w-[100vw] mt-0 ld:mt-16 flex flex-col min-[320px]:max-sm:justify-center min-[320px]:max-sm:items-center gap-x-2 sm:flex-row xl:ml-12">
         <div className="min-w-[10%] md:w-[23%]">
           <LandlordDashboardSidebar />
         </div>
 
-        <div className=" sm:ml-0 md:w-[65%] md:mx-auto xl:ml-3">
+        <div className=" sm:ml-0 w-[90%] md:w-[65%] md:mx-auto xl:ml-3">
           <Outlet />
         </div>
       </div>
