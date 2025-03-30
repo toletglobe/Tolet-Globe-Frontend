9f93f41aeb063c33e721d107ac0bd5691e5e2004
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-03-31 01:27:36 +0530

    changes push

--- src/pages/Dashboard/landlord/LandlordDashboard.jsx
+++ src/pages/Dashboard/landlord/LandlordDashboard.jsx
@@ -8,12 +8,6 @@ export default function LandlordDashboard() {
             <div className="min-w-[10%] md:w-[23%]">
                <LandlordDashboardSidebar />
             </div>
-  return (
-    <>
-      <div className="w-[100vw] mt-0 ld:mt-16 flex flex-col min-[320px]:max-sm:justify-center min-[320px]:max-sm:items-center gap-x-2 sm:flex-row xl:ml-12">
-        <div className="min-w-[10%] md:w-[23%]">
-          <LandlordDashboardSidebar />
-        </div>
 
             <div className=" sm:ml-0 md:w-[65%] md:mx-auto xl:ml-3">
                <Outlet />
