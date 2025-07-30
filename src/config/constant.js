1c5492fadaaae4657768eb5161750fc1afbe88c5
Addiyy <mad.itya1802@gmail.com>
2025-07-31 01:30:33 +0530

    Filter Modified and Hover enabled

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://to-let-backend.onrender.com/api/v1/";
+//export const BASE_URL = "https://to-let-backend.onrender.com/api/v1/";
 
-// export const BASE_URL = "http://localhost:8000/api/v1/";
+export const BASE_URL = "http://localhost:8000/api/v1/";
 
 // export const BASE_URL = "https://13.232.171.196:8000/api/v1/";
